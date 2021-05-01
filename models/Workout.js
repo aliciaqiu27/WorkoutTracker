9fcad0f719f26bb5818072d06fe0cdc3df495038
Alicia Qiu Cheung <aliciaqiu27@gmail.com>
2021-05-01 16:54:42 -0700

    added all categories under workout.js file

--- models/Workout.js
+++ models/Workout.js
@@ -2,11 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const BookSchema = new Schema({
-  author: String,
-  title: String
+const WorkoutTrackerSchema = new Schema({
+  day: Date,
+  exercises: [
+    {
+      type: {
+        type: String,
+        required:"Exercise type is required!",
+        trim: true,
+      },
+      name: {
+        type: String,
+        required: "Name of exercise is required!",
+        trim: true,
+      },
+      duration: {
+        type: Integer,
+        required: "Duration of exercise is required!",
+      },
+      weight: {
+        type: Integer,
+        required: "Weight is required!",
+      },
+      reps: {
+        type: Integer,
+        required: "Number of reps is required!",
+      },
+      sets: {
+        type: Integer,
+        required: "Number of sets is required!",
+      },
+      distance: {
+        type: Integer,
+      },
+    },
+  ],
 });
 
-const Book = mongoose.model("Book", BookSchema);
+const WorkoutTracker = mongoose.model("WorkoutTracker", WorkoutTrackerSchema);
 
-module.exports = Book;
+module.exports = WorkoutTracker;
